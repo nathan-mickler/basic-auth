bf095ef35c9adf8a8eee5abb7840cf4015d45756
Nathan Mickler <nathan@iclasspro.com>
2024-11-09 16:44:38 +0300

    Adding basic JWT auth (sanctum personal auth tokens)

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enums\TokenAbility;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $name = $request->post('name');
+        $email = $request->post('email');
+        $password = $request->post('password');
+
+        $data = [
+            'name' => $name,
+            'email' => $email,
+            'password' => $password
+        ];
+
+        $user = User::create($data);
+        $accessToken = $user->createToken('access_token', [TokenAbility::ACCESS_API->value], Carbon::now()->addMinutes(config('sanctum.ac_expiration')));
+        $refreshToken = $user->createToken('refresh_token', [TokenAbility::ISSUE_ACCESS_TOKEN->value], Carbon::now()->addMinutes(config('sanctum.rt_expiration')));
+
+        return [
+            'accessToken' => $accessToken->plainTextToken,
+            'refreshToken' => $refreshToken->plainTextToken,
+        ];
+    }
+
+    public function login(Request $request)
+    {
+        $email = $request->post('email');
+        $password = $request->post('password');
+
+        if (!auth()->attempt(['email' => $email, 'password' => $password])) {
+            return response(['message' => 'Invalid credentials'], 401);
+        }
+
+        $accessToken = auth()->user()->createToken('access_token', [TokenAbility::ACCESS_API->value], Carbon::now()->addMinutes(config('sanctum.ac_expiration')));
+        $refreshToken = auth()->user()->createToken('refresh_token', [TokenAbility::ISSUE_ACCESS_TOKEN->value], Carbon::now()->addMinutes(config('sanctum.rt_expiration')));
+
+        return [
+            'accessToken' => $accessToken->plainTextToken,
+            'refreshToken' => $refreshToken->plainTextToken,
+        ];
+    }
+
+    public function refreshToken(Request $request)
+    {
+        $accessToken = $request->user()->createToken('access_token', [TokenAbility::ACCESS_API->value], Carbon::now()->addMinutes(config('sanctum.ac_expiration')));
+        return response(['message' => "Token generated", 'token' => $accessToken->plainTextToken]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Enums\TokenAbility;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum, ability:' . TokenAbility::ISSUE_ACCESS_TOKEN->value])
+    ->group(function () {
+        Route::get('/auth/refresh', [AuthController::class, 'refreshToken']);
+});
